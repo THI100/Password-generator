7a4beaced0b3d6e039da5e571538c58d41edf1d3
THI100 <thiagocas100@gmail.com>
2025-08-11 22:20:42 -0300

    Added security levels and the function working with it. The whole project is in initial levels.

--- Creator.py
+++ Creator.py
@@ -1,2 +1,13 @@
-def MakePassword ():
-    print("MakePassword function called")
+def makePassword (SecurityLevel: str):
+    print(f"MakePassword function called with security level: {SecurityLevel}")
+
+    if SecurityLevel == "Lw":
+        print("Low level selected")
+    elif SecurityLevel == "Md":
+        print("Medium level selected")
+    elif SecurityLevel == "Hg":
+        print("High level selected")
+    elif SecurityLevel == "Ex":
+        print("Extreme level selected")
+    else:
+        print("Error: Invalid security level")
--- Interface.py
+++ Interface.py
@@ -1,15 +1,24 @@
-from Creator import MakePassword
+import Creator
 import tkinter as tk
 
 root = tk.Tk()
 root.title("Password Generator")
-root.geometry("400x300")
+root.geometry("800x600")
 
 title = tk.Label(root, text="Password Generator", font=("Arial", 24))
 title.pack(pady=20)
 stc = tk.Label(root, text="Select the configurations:", font=("Arial", 18))
 stc.pack(pady=20)
-confirm = tk.Button(root, text="Confirm", default="active", command=MakePassword)
+
+security_level = tk.Label(root, text="Security Level:", font=("Arial", 14))
+security_level.pack(pady=10)
+SecurityLvl = tk.StringVar(value="Md") 
+select_level_low = tk.Radiobutton(root, text='Low', value='Lw', variable=SecurityLvl).pack(anchor='w')
+select_level_medium = tk.Radiobutton(root, text='Medium', value='Md', variable=SecurityLvl).pack(anchor='w')
+select_level_high = tk.Radiobutton(root, text='High', value='Hg', variable=SecurityLvl).pack(anchor='w')
+select_level_extreme = tk.Radiobutton(root, text='Extreme', value='Ex', variable=SecurityLvl).pack(anchor='w')
+
+confirm = tk.Button(root, text="Confirm", default="active", command=Creator.makePassword(SecurityLvl.get()))
 confirm.pack(pady=20)
 
 root.mainloop()
